7621ba2ea36416e0de8b1f07992a461c65d01a8c
Francois Kiesecoms <kiesecoms.francois@gmail.com>
2018-10-29 15:54:56 +0100

    cleaner

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,23 +12,20 @@ class GamesController < ApplicationController
     url = "https://wagon-dictionary.herokuapp.com/#{@word}"
     a = open(url).read
     data_hash = JSON.parse(a)
-    letter_hash = letter_count(params[:letters].split(" ").join)
-    word_hash = letter_count(@word)
-    if (data_hash["found"] == true) && (word_hash.all? { |let, freq| letter_hash[let] >= freq })
-      @score = "all ok"
-    elsif (data_hash["found"] == true) && !(word_hash.all? { |let, freq| letter_hash[let] >= freq })
-      @score = "valid but not using all good letters"
-    elsif !(data_hash["found"] == true) && (word_hash.all? { |let, freq| letter_hash[let] >= freq })
-      @score = "not valid"
+    # letter_hash = Hash.new(0)
+    letters = params[:letters].split(" ").join
+    # word_hash = letter_count(@word)
+    if (data_hash["found"] == false)
+      @score = "not an english word"
+    elsif @word.chars.all? { |letter| @word.count(letter) <= letters.count(letter) } == false
+      @score = "letters not in grid"
+    else
+      @score = "Well Done"
     end
-  end
-
-  def letter_count(str)
-    str.downcase.each_char.with_object({}) { |c,h|
-      (h[c] = h.fetch(c,0) + 1) if c =~ /[a-z]/ }
   end
 end
 
 # passing instance variables between requests is impossible... controller actions backing different request
+
 
 
